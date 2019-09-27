307d0770f6648dc3063afb60d165af279fe4a6f9
Lucio Verane <verane64@gmail.com>
2019-09-26 23:05:59 -0500

    css improved, placeholder questions/answers replaced, restart function/option added, buttons working properly in all cases

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,129 +1,146 @@
 $(document).ready(function () {
 
-var triviaDatabase = [
-    {
-        question: "A is right",
-        answers: [
-            "first answer",
-            "second answer",
-            "third answer"
-        ],
-        correctAnswer: "0",
-    },
-    {
-        question: "C is right",
-        answers: [
-            "first answer",
-            "second answer",
-            "third answer"
-        ],
-        correctAnswer: "2",
-    },
-    {
-        question: "B is right",
-        answers: [
-            "first answer",
-            "second answer",
-            "third answer"
-        ],
-        correctAnswer: "1",
-    },
-    {
-        question: "C is right",
-        answers: [
-            "first answer",
-            "second answer",
-            "third answer"
-        ],
-        correctAnswer: "2",
-    },
-    {
-        question: "B is right",
-        answers: [
-            "first answer",
-            "second answer",
-            "third answer"
-        ],
-        correctAnswer: "1",
-    }
-]
-
-var right = 0; //need to reset this after the result is presented //tried to only create it inside the function and it didn't work
-var time = 60; //maybe I have to reset it somewhere?
-var intervalId;
-var clockRunning = false;
-
-//creating the html for the trivia
-function generateHtml(){
-    $("#score").html("");
-    for(i=0; i<triviaDatabase.length; i++){
-        var newQuestion = $("<h4>").append(triviaDatabase[i].question);
-        $("#allQuestions").append(newQuestion);
-        var newAnswer = $('<div id=question' + i + '>');
-
-        for(j=0; j<3; j++){//replace 3 by a variable down the line
-            
-            newAnswer.append('<input type=radio name=answer' + i + ' value=' + j + '>' + triviaDatabase[i].answers[j]);
-            console.log(triviaDatabase[i].answers[j]);
-            
-            $("#allQuestions").append(newAnswer);
+    var triviaDatabase = [
+        {
+            question: "Who is the strongest green hero?",
+            answers: [
+                "Shrek",
+                "Hulk",
+                "Green Lantern"
+            ],
+            correctAnswer: "0",
+        },
+        {
+            question: "When asking for a favor, which phrasing should be used?",
+            answers: [
+                "Could you please?",
+                "Do you mind?",
+                "Would you kindly?"
+            ],
+            correctAnswer: "2",
+        },
+        {
+            question: "What is the property of a Poring?",
+            answers: [
+                "Fire",
+                "Water",
+                "Wind"
+            ],
+            correctAnswer: "1",
+        },
+        {
+            question: "Who is the Rickest Rick?",
+            answers: [
+                "J-22",
+                "D-716",
+                "C-137"
+            ],
+            correctAnswer: "2",
+        },
+        {
+            question: "Fish are ___",
+            answers: [
+                "Food",
+                "Friends",
+                "Holograms"
+            ],
+            correctAnswer: "1",
         }
+    ];
 
-        console.log(newQuestion);
+    var unanswered = 0;
+    var right = 0;
+    var wrong = 0;
+    var time = 60;
+    var intervalId;
+    var clockRunning = false;
+
+    function reset() {
+        time = 60;
+        unanswered = 0;
+        wrong = 0;
+        right = 0;
+        $("#allQuestions").html('');
+        $("#score").html('');
+        $("#timeLeft").html('');
     }
-}
 
-//check if user answers are right
-function calculateScore (){
-    for(i=0; i<triviaDatabase.length; i++){
-        var userAnswer = $('#question' + i).children("input:checked").val();
-        console.log("userAnswer: " + userAnswer);
-        console.log("trivia[i].answer: " + triviaDatabase[i].correctAnswer);
-        if(userAnswer === triviaDatabase[i].correctAnswer){
-            right++;
-            console.log("right: " + right);
+    //creating the html for the trivia
+    function generateHtml() {
+        $("#instruction").html('Select the right answers and click Submit');
+        $("#score").html("");
+        for (i = 0; i < triviaDatabase.length; i++) {
+            var newQuestion = $("<h2>").append(triviaDatabase[i].question);
+            $("#allQuestions").append(newQuestion);
+            var newAnswer = $('<div class=answerOption id=question' + i + '>');
+
+            for (j = 0; j < triviaDatabase[i].answers.length; j++) {
+                newAnswer.append('<input type=radio name=answer' + i + ' value=' + j + '>' + triviaDatabase[i].answers[j]);
+                $("#allQuestions").append(newAnswer);
+            }
         }
     }
-    $("#score").html("You got " + right + " right answers!")
-    right = 0;
-}
 
-function run() {
-    if (!clockRunning) {// prevents multiple instances of the clock running at the same time
-        clockRunning = true;
-        $("#timeLeft").html("60");
-        intervalId = setInterval(decrement, 1000);
+    function calculateScore() {
+        for (i = 0; i < triviaDatabase.length; i++) {
+            var userAnswer = $('#question' + i).children("input:checked").val();
+            if (userAnswer === triviaDatabase[i].correctAnswer) {
+                right++;
+            } else if (typeof userAnswer === 'undefined') {
+                unanswered++;
+            } else {
+                wrong++;
+            }
+        }
+        $("#score").append('<h4> You got ' + right + ' right answers! </h4>')
+        $("#score").append('<h4> You got ' + wrong + ' wrong answers! </h4>')
+        if (unanswered > 0) {
+            $("#score").append('<h4> You failed to answer ' + unanswered + ' questions!</h4>');
+        }
+        $("#allQuestions").html('');
+        $("#start").html('Start');
+        $("#instruction").html('Press Start to Begin the Quizz!');
     }
-}
 
-function decrement() {
-    time--;
-    $("#timeLeft").html(time);
+    function run() {
+        if (!clockRunning) {// prevents multiple instances of the clock running at the same time
+            clockRunning = true;
+            $("#timeLeft").html("60");
+            intervalId = setInterval(decrement, 1000);
+        }
+    }
 
-    if (time === 0) {
+    function decrement() {
+        time--;
+        $("#timeLeft").html(time);
+
+        if (time === 0) {
+            $("#submit").hide();
+            stop();
+            calculateScore();
+        }
+    }
+
+    function stop() {
+        clearInterval(intervalId);
+        clockRunning = false;
+    }
+
+    $("#start").on("click", function () {
+        reset();
+        $("#start").html('Restart');
+        $("#allQuestions").html("");
+        generateHtml();
+        $("#submit").show();
+        run();
+    });
+
+    $("#submit").on("click", function () {
+        $("#submit").hide();
+        $("#timeLeft").html('');
         stop();
         calculateScore();
-    }
-}
 
-function stop() {
-    clearInterval(intervalId);
-    clockRunning = false;
-}
-
-//start. set for timer now. might have to call function that makes the html trivia
-$("#start").on("click", function () {
-    $("#allQuestions").html("");
-    generateHtml();
-    run();
-    // $("#submit").html('<button id=submitButton>Submit</button>'); //not working for some reason
-});
-
-// make a function to check and count "right" and just call it with .click
-$("#submitButton").on("click", function () {
-    stop();
-    calculateScore();
-
-});
+    });
 
 });
